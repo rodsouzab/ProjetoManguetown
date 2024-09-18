2cd57c915a85c6d105c9d42887f1301091a8f079
rodsouzab <rodrigosouza.souzabarbosa1@gmail.com>
2024-09-17 21:06:55 -0300

    Criação de Environment e Models de Colaboradores

---
+++ ProjetoManguetown/app/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class Colaborador(models.Model):
+    nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=14, unique=True)
+    data_nascimento = models.DateField(null=True, blank=True)
+    lugar_onde_mora = models.CharField(max_length=255)
+    renda = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    situacoes_de_vulnerabilidade = models.TextField(blank=True, null=True)
+    
+    quantos_filhos = models.PositiveIntegerField()
+    quantas_pessoas_moram_com_voce = models.PositiveIntegerField()
+    habilidades = models.TextField()
+
+    def __str__(self):
+        return self.nome
