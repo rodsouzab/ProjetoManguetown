27f07f27c11ded134b64dc7662dcd2079a11851f
rodsouzab <rodrigosouza.souzabarbosa1@gmail.com>
2024-09-24 13:28:46 -0300

    Tela de login não funcional e transição de telas

--- ProjetoManguetown/app/views.py
+++ ProjetoManguetown/app/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth import views as auth_views
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
+
+def sobre(request):
+    return render(request, 'sobre.html')
+
+def contato(request):
+    return render(request, 'contato.html')
+
+class LoginView(auth_views.LoginView):
+    template_name = 'login.html'
