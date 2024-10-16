5b96b017df47346306b20d3c802158c6e72ce4db
MatheusLustosa <matheusclc@hotmail.com>
2024-10-16 16:47:42 -0300

    cadastro empresa

--- app/admin.py
+++ app/admin.py
@@ -3,3 +3,8 @@ from django.contrib import admin
 from .models import Colaborador
 
 admin.site.register(Colaborador)
+
+from .models import EmpresaParceira  # Importe o modelo EmpresaParceira
+
+# Registre o modelo EmpresaParceira no admin
+admin.site.register(EmpresaParceira)
