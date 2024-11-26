8c81df710412c6d5298b66df53b44d7cfb2a622a
rodsouzab <rodrigosouza.souzabarbosa1@gmail.com>
2024-11-26 19:50:31 -0300

    mensagem de sessão expirada

--- app/middlewares.py
+++ app/middlewares.py
@@ -1,6 +1,7 @@
 
 from django.urls import reverse
 from django.shortcuts import redirect
+from django.contrib import messages
 
 class SessionTimeoutMiddleware:
     def __init__(self, get_response):
@@ -8,5 +9,6 @@ class SessionTimeoutMiddleware:
 
     def __call__(self, request):
         if not request.user.is_authenticated and request.path != reverse('manguetown:login'):
+            messages.error(request, 'Sua sessão foi encerrada. Faça o login novamente')
             return redirect('manguetown:login')
         return self.get_response(request)
