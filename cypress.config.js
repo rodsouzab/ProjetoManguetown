896937bfa1b25fb9919cd7e0c81e3aaf40e8369e
rodsouzab <rodrigosouza.souzabarbosa1@gmail.com>
2024-11-26 19:20:38 -0300

    settings changes

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,10 @@ module.exports = defineConfig({
     baseUrl: "https://projetomanguetown-app.azurewebsites.net",
     //Para testes locais:
     //baseUrl: 'http://localhost:8000',
+    retries: {
+      runMode: 2, // Número de retries durante execução do teste
+      openMode: 0 // Número de retries no modo interativo
+    },
     viewportWidth: 1920,
     viewportHeight: 1080,
     watchForFileChanges: false,
